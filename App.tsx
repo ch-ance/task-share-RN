fc8d81a0135056a817cf8e66d784e56c82091e57
halo <cembreyfarquhar@gmail.com>
2019-09-06 22:46:12 -0500

    adds packages for react-navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { AuthProvider } from 'react-use-auth'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Is it really fucking working?????</Text>
     </View>
   );
 }
