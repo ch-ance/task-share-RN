e97cdc97d67ef281de43d8949b3e61108cfddf59
halo <cembreyfarquhar@gmail.com>
2019-09-06 23:25:22 -0500

    react-navigation demo complete - chore: experiment with auth

--- App.tsx
+++ App.tsx
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import { AuthProvider } from 'react-use-auth'
+// import { AuthProvider } from 'react-use-auth'
 
-export default function App() {
+const HomeScreen = props => {
+  const { navigation } = props;
+  const navigationOptions = {
+    title: "Home"
+  };
   return (
-    <View style={styles.container}>
-      <Text>Is it really fucking working?????</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "red"
+      }}
+    >
+      <Text>Home Screen Component</Text>
+      <Button title="hey" onPress={() => navigation.navigate("Details")} />
     </View>
   );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
   },
-});
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
